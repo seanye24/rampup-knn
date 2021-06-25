9b32595905dde7557e467ffac5f36de2d34f0787
Sean Ye <yesean@amazon.com>
2021-06-25 12:56:50 -0700

    Update test scripts

--- test-dimension.sh
+++ test-dimension.sh
@@ -6,6 +6,6 @@ echo "k: 4"
 for i in {3..20}; do
   sh ./generate-input.sh 10 5 $((2 ** i))
   echo -n "dimensions: $((2 ** i)), "
-  python main.py index.csv query.csv l2 4 output.csv
+  python main.py index.csv query.csv l2 4 output.csv --verbose
   echo
 done
--- test-index.sh
+++ test-index.sh
@@ -6,6 +6,6 @@ echo "k: 4"
 for i in {3..20}; do
   sh ./generate-input.sh $((2 ** i)) 5 5
   echo -n "number of index vectors: $((2 ** i)), "
-  python main.py index.csv query.csv l2 4 output.csv
+  python main.py index.csv query.csv l2 4 output.csv --verbose
   echo
 done
