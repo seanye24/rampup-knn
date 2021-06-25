750e7f43d7c7bf2d6de86f395eab242ab5c0abb1
Sean Ye <yesean@amazon.com>
2021-06-24 18:13:25 -0700

    Add IOReader

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 def l2(p, q):
     ''' Takes two vectors, and calculates their distance in l2 space '''
     return sum(((pi - qi) ** 2 for pi, qi in zip(p, q))) ** 0.5
@@ -24,11 +26,21 @@ class Index:
         space_f = spaces[space]
         results = [(iv, space_f(iv, query_vector)) for iv in self.__index_vectors]
 
+        # select the k-nearest vectors
         nearest_vectors = []
-        for i in range(k):
+        for _ in range(k):
             min_vector, min_vector_dist = results[0]
             for iv, iv_dist in results:
                 if not iv in nearest_vectors and iv_dist < min_vector_dist:
                     min_vector, min_vector_dist = iv, iv_dist
             nearest_vectors.append(min_vector)
         return nearest_vectors
+
+class IOReader:
+    '''Reads data from csv and creates an Index'''
+
+    def read_index(self, csv_path):
+        with open(csv_path, newline='') as csvfile:
+            vector_reader = csv.reader(csvfile)
+            index_vectors = [tuple(vector) for vector in vector_reader]
+            return Index(index_vectors)
